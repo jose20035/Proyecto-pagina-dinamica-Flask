87d8f8038b39963c1fbef9c7b27c30f04c9ab229
José Miguel Calderon Frutos <josemiguelcalderonfrutos@gmail.com>
2020-04-17 12:28:25 +0200

    Creacion de la pagina Potencias

--- app.py
+++ app.py
@@ -3,6 +3,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return render_template("index.html")
+    return render_template("Principal.html")
+
+@app.route('/potencia/<base>/<exponente>')
+def potencia(base,exponente):
+    base=int(base)
+    exponente=int(exponente)
+    if exponente == 0:
+        resultado=1
+    elif exponente < 0:
+        resultado=(1/base)**abs(exponente)
+    elif exponente > 0:
+        resultado=base**exponente
+    else:
+        abort(404)
+    return render_template("Potencias.html",base=base,exponente=exponente,resultado=resultado)
 
 app.run(debug=True)
